ce8beeee71498e5b9c1383c8aac5245b18531d14
Luke Childs <lukechilds123@gmail.com>
2016-04-21 21:55:42 +0100

    Add tests for Urls()

--- test/unit.js
+++ test/unit.js
@@ -1,1 +1,17 @@
 var expect = require('chai').expect;
+
+var Urls = require('../dist/my-name-is-url');
+var Parser = require('../dist/parser');
+var regex = require('../dist/regex');
+
+describe('Urls()', function() {
+
+  it('Should return instance of parser', function () {
+    expect(Urls()).to.be.an.instanceof(Parser);
+  });
+
+  it('Should expose regex as property', function () {
+    expect(Urls.regex).to.equal(regex);
+  });
+
+});
