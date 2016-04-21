3eb0be56cbfbe8b5d26bd228aca3d1a068609826
Luke Childs <lukechilds123@gmail.com>
2016-04-21 23:53:39 +0100

    Add filter() method

--- src/parser.js
+++ src/parser.js
@@ -18,4 +18,20 @@ export default class Urls {
     return this.text.match(regex) || [];
   }
 
+  filter(cb) {
+
+    // Check we've got something we can parse
+    if(typeof this.text !== 'string') {
+      return '';
+    }
+
+    // Check callback is a funciton
+    if(typeof cb !== 'function') {
+      throw new Error('Invalid filter callback');
+    }
+
+    // Run filter on urls
+    return this.text.replace(regex, cb);
+  }
+
 }
