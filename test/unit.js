7a15ffbdd80c8429075f68ee952eea3cf6ac64a0
Luke Childs <lukechilds123@gmail.com>
2016-04-21 22:12:24 +0100

    Add matches json to test

--- test/unit.js
+++ test/unit.js
@@ -4,6 +4,8 @@ var Urls = require('../dist/my-name-is-url');
 var Parser = require('../dist/parser');
 var regex = require('../dist/regex');
 
+var matches = require('./matches.json');
+
 describe('Urls()', function() {
 
   it('Should return instance of parser', function () {
@@ -21,6 +23,16 @@ describe('Urls()', function() {
       expect(Urls('').get()).to.be.instanceof(Array);
       expect(Urls('no url').get()).to.be.instanceof(Array);
       expect(Urls('url.com').get()).to.be.instanceof(Array);
+    });
+
+  });
+
+  describe('Should match', function() {
+
+    matches.forEach(function(url) {
+      it(url, function () {
+        expect(Urls(url).get()).to.deep.equal([url]);
+      });
     });
 
   });
