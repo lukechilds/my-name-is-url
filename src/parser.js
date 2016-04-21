d300a1319a13392e981437064a026d6d85adea01
Luke Childs <lukechilds123@gmail.com>
2016-04-21 21:49:33 +0100

    Add get() method

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,14 @@
+import regex from './regex';
+
 // Factory function to return parser instance
 export default class Urls {
 
   constructor(text = null) {
     this.text = text;
+  }
+
+  get() {
+    return this.text.match(regex);
   }
 
 }
