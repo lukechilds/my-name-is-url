caca02ea52793e73a152ad3083bf127e20628c67
Luke Childs <lukechilds123@gmail.com>
2016-04-23 10:50:46 +0100

    Check urls on init too

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Urls from 'my-name-is-url';
 
 const testText = document.querySelector('.test-text');
 
-testText.addEventListener('input', () => {
+function checkUrls() {
   const urls = Urls(testText.innerText).get();
   console.log(urls);
-});
+}
+checkUrls();
+testText.addEventListener('input', checkUrls);
