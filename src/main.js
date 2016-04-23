6c584f25a84de0ba94c00ec5302db7688b07567a
Luke Childs <lukechilds123@gmail.com>
2016-04-23 10:27:38 +0100

    Log urls in string

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import Urls from 'my-name-is-url';
+
+const testText = document.querySelector('.test-text');
+
+testText.addEventListener('input', () => {
+  const urls = Urls(testText.innerText).get();
+  console.log(urls);
+});
