f39760f9bd3ac5871f1caf3cdd3450ff8a1fa998
Luke Childs <lukechilds123@gmail.com>
2016-04-27 23:38:55 +0100

    Make urls in double quotes grabbable

--- src/regex.js
+++ src/regex.js
@@ -3,18 +3,18 @@ import tlds from 'tlds';
 // Reusables
 const validTlds = tlds.concat(['local', 'dev']).join('|');
 const escapeChar = `\\`;
-const notWhitespaceHTMLOrComma = `[^${escapeChar}s<>,]`;
+const notWhitespaceHTMLDoubleQuoteOrComma = `[^${escapeChar}s<>",]`;
 const notWhitespaceCommaOrDot = `[^${escapeChar}s,.]`;
-const WhitespaceCommaDotHTMLOrEndOfLine = `[${escapeChar}s,.<>]|$`;
+const WhitespaceCommaDotHTMLDoubleQuoteOrEOL = `[${escapeChar}s,.<>"]|$`;
 const number = `[0-9]`;
 
 // Sections
 const optionalScheme = `((https?:)?//)?`;
-const hostname = `(${notWhitespaceHTMLOrComma}+${escapeChar}.(${validTlds}))`;
+const hostname = `(${notWhitespaceHTMLDoubleQuoteOrComma}+${escapeChar}.(${validTlds}))`;
 const ip = `(${number}{1,3}${escapeChar}.){3}${number}{1,3}`;
 const optionalPortNumber = `(:${number}+)?`;
 const optionalSlash = `(${escapeChar}/(${notWhitespaceCommaOrDot}*)?)?`;
-const endsWithButDontMatch = `(?=${WhitespaceCommaDotHTMLOrEndOfLine})`;
+const endsWithButDontMatch = `(?=${WhitespaceCommaDotHTMLDoubleQuoteOrEOL})`;
 
 // Build
 const regex = `${optionalScheme}(localhost|${hostname}|${ip})${optionalPortNumber}${optionalSlash}${endsWithButDontMatch}`;
