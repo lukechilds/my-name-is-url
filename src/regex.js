68a587976a17d373318e0a821d6e8cc867f21f22
Luke Childs <lukechilds123@gmail.com>
2016-04-27 23:14:33 +0100

    Match optional scheme for any pattern

--- src/regex.js
+++ src/regex.js
@@ -9,6 +9,7 @@ const WhitespaceCommaDotOrEndOfLine = `[${escapeChar}s,.]|$`;
 const number = `[0-9]`;
 
 // Sections
+const optionalScheme = `((https?:)?//)?`;
 const hostname = `(${notWhitespaceOrComma}+${escapeChar}.(${validTlds}))`;
 const ip = `(${number}{1,3}${escapeChar}.){3}${number}{1,3}`;
 const optionalPortNumber = `(:${number}+)?`;
@@ -16,6 +17,6 @@ const optionalSlash = `(${escapeChar}/(${notWhitespaceCommaOrDot}*)?)?`;
 const endsWithButDontMatch = `(?=${WhitespaceCommaDotOrEndOfLine})`;
 
 // Build
-const regex = `(localhost|${hostname}|${ip})${optionalPortNumber}${optionalSlash}${endsWithButDontMatch}`;
+const regex = `${optionalScheme}(localhost|${hostname}|${ip})${optionalPortNumber}${optionalSlash}${endsWithButDontMatch}`;
 
 export default new RegExp(regex, 'gi');
