18eb648178a66a147f81ebe017c6348066a2bf8f
Luke Childs <lukechilds123@gmail.com>
2016-05-01 12:58:20 +0100

    Don't allow dots in hostname

--- src/regex.js
+++ src/regex.js
@@ -3,14 +3,14 @@ import tlds from 'tlds';
 // Reusables
 const validTlds = tlds.concat(['local', 'dev']).join('|');
 const escapeChar = `\\`;
-const notWhitespaceHTMLDoubleQuoteOrComma = `[^${escapeChar}s<>",]`;
+const notWhitespaceHTMLDoubleQuoteDotOrComma = `[^${escapeChar}s<>".,]`;
 const notWhitespaceCommaDoubleQuoteOrDot = `[^${escapeChar}s,".]`;
 const WhitespaceCommaDotHTMLDoubleQuoteOrEOL = `[${escapeChar}s,.<>"]|$`;
 const number = `[0-9]`;
 
 // Sections
 const optionalScheme = `((https?:)?//)?`;
-const hostname = `(${notWhitespaceHTMLDoubleQuoteOrComma}+${escapeChar}.(${validTlds}))`;
+const hostname = `(${notWhitespaceHTMLDoubleQuoteDotOrComma}+${escapeChar}.(${validTlds}))`;
 const ip = `(${number}{1,3}${escapeChar}.){3}${number}{1,3}`;
 const optionalPortNumber = `(:${number}+)?`;
 const optionalSlash = `(${escapeChar}/(${notWhitespaceCommaDoubleQuoteOrDot}*)?)?`;
