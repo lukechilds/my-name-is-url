fe446b4dbbfdf9ba08e92a674f107d5146711b79
Luke Childs <lukechilds123@gmail.com>
2016-05-02 18:35:31 +0100

    Redo hostname/subdomain regex to handle dashes properly
    Previous version just passed tests by chance but actually allowed any char hence the added test for hostname ending with space

--- src/regex.js
+++ src/regex.js
@@ -5,13 +5,13 @@ const validTlds = tlds.concat(['local', 'dev']).join('|');
 const escapeChar = `\\`;
 const notWhitespaceCommaDoubleQuoteOrDot = `[^${escapeChar}s,".]`;
 const dot = `${escapeChar}.`;
-const alphabetical = `${escapeChar}w`;
+const hostnameChars = `[a-z0-9]`;
 const number = `[0-9]`;
 const endingChars = `${dot}?([${escapeChar}s<>",]|$)`;
 
 // Sections
-const optionalScheme = `((${alphabetical}+:)?//)?`;
-const hostname = `((${alphabetical}{1}([${alphabetical}-]*([^-]|${alphabetical}))?${dot})+(${validTlds}))`;
+const optionalScheme = `(([a-z]+:)?//)?`;
+const hostname = `(((${hostnameChars}-*)*${hostnameChars}+)${dot})+(${validTlds})`;
 const ip = `(${number}{1,3}${dot}){3}${number}{1,3}`;
 const optionalPortNumber = `(:${number}+)?`;
 const optionalSlash = `(${escapeChar}/(${notWhitespaceCommaDoubleQuoteOrDot}*)?)?`;
