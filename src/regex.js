ef77617dc2903c7e298077e9ac0ba8f58be80e4b
Luke Childs <lukechilds123@gmail.com>
2016-05-02 13:54:24 +0100

    Stricter checking of hostname for hyphons and other invalid chars

--- src/regex.js
+++ src/regex.js
@@ -3,19 +3,20 @@ import tlds from 'tlds';
 // Reusables
 const validTlds = tlds.concat(['local', 'dev']).join('|');
 const escapeChar = `\\`;
-const notWhitespaceHTMLDoubleQuoteDotOrComma = `[^${escapeChar}s<>".,]`;
+const containingChars = `${escapeChar}s<>".,`;
+const notContainingChars = `[^${containingChars}]`;
 const notWhitespaceCommaDoubleQuoteOrDot = `[^${escapeChar}s,".]`;
-const WhitespaceCommaDotHTMLDoubleQuoteOrEOL = `[${escapeChar}s,.<>"]|$`;
+const containingCharsOrEOL = `[${containingChars}]|$`;
 const number = `[0-9]`;
 
 // Sections
 const optionalScheme = `((https?:)?//)?`;
-const optionalSubdomain = `(${notWhitespaceHTMLDoubleQuoteDotOrComma}+${escapeChar}.)?`;
-const hostname = `(${notWhitespaceHTMLDoubleQuoteDotOrComma}+${escapeChar}.(${validTlds}))`;
+const optionalSubdomain = `(${notContainingChars}+${escapeChar}.)?`;
+const hostname = `([^${escapeChar}W]{1}[${escapeChar}w-]*[^-]${escapeChar}.(${validTlds}))`;
 const ip = `(${number}{1,3}${escapeChar}.){3}${number}{1,3}`;
 const optionalPortNumber = `(:${number}+)?`;
 const optionalSlash = `(${escapeChar}/(${notWhitespaceCommaDoubleQuoteOrDot}*)?)?`;
-const endsWithButDontMatch = `(?=${WhitespaceCommaDotHTMLDoubleQuoteOrEOL})`;
+const endsWithButDontMatch = `(?=${containingCharsOrEOL})`;
 
 // Build
 const regex = `${optionalScheme}${optionalSubdomain}(localhost|${hostname}|${ip})${optionalPortNumber}${optionalSlash}${endsWithButDontMatch}`;
