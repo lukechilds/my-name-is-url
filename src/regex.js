99df1b19b76ea8c9efeca434388bb48dcd534f9e
Luke Childs <lukechilds123@gmail.com>
2016-05-02 13:55:26 +0100

    Repeat hostname check for subdomains

--- src/regex.js
+++ src/regex.js
@@ -4,21 +4,19 @@ import tlds from 'tlds';
 const validTlds = tlds.concat(['local', 'dev']).join('|');
 const escapeChar = `\\`;
 const containingChars = `${escapeChar}s<>".,`;
-const notContainingChars = `[^${containingChars}]`;
 const notWhitespaceCommaDoubleQuoteOrDot = `[^${escapeChar}s,".]`;
 const containingCharsOrEOL = `[${containingChars}]|$`;
 const number = `[0-9]`;
 
 // Sections
 const optionalScheme = `((https?:)?//)?`;
-const optionalSubdomain = `(${notContainingChars}+${escapeChar}.)?`;
-const hostname = `([^${escapeChar}W]{1}[${escapeChar}w-]*[^-]${escapeChar}.(${validTlds}))`;
+const hostname = `(([^${escapeChar}W]{1}[${escapeChar}w-]*[^-]${escapeChar}.)+(${validTlds}))`;
 const ip = `(${number}{1,3}${escapeChar}.){3}${number}{1,3}`;
 const optionalPortNumber = `(:${number}+)?`;
 const optionalSlash = `(${escapeChar}/(${notWhitespaceCommaDoubleQuoteOrDot}*)?)?`;
 const endsWithButDontMatch = `(?=${containingCharsOrEOL})`;
 
 // Build
-const regex = `${optionalScheme}${optionalSubdomain}(localhost|${hostname}|${ip})${optionalPortNumber}${optionalSlash}${endsWithButDontMatch}`;
+const regex = `${optionalScheme}(localhost|${hostname}|${ip})${optionalPortNumber}${optionalSlash}${endsWithButDontMatch}`;
 
 export default new RegExp(regex, 'gi');
