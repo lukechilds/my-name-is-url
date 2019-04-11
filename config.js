2b3b416829089380565d3298c37d2a71fde47042
Luke Childs <lukechilds123@gmail.com>
2019-04-11 15:59:04 +0700

    Update my-name-is-url to v1.4.0

--- config.js
+++ config.js
@@ -18,12 +18,12 @@ System.config({
       "npm:strict-uri-encode@1.1.0/index.js",
       "npm:escape-html@1.0.3.js",
       "npm:escape-html@1.0.3/index.js",
-      "npm:my-name-is-url@1.3.1.js",
-      "npm:my-name-is-url@1.3.1/dist/my-name-is-url.js",
-      "npm:my-name-is-url@1.3.1/dist/regex.js",
-      "npm:tlds@1.111.0.js",
-      "npm:tlds@1.111.0/index.js",
-      "npm:my-name-is-url@1.3.1/dist/parser.js"
+      "npm:my-name-is-url@1.4.0.js",
+      "npm:my-name-is-url@1.4.0/dist/my-name-is-url.js",
+      "npm:my-name-is-url@1.4.0/dist/regex.js",
+      "npm:tlds@1.203.1.js",
+      "npm:tlds@1.203.1/index.js",
+      "npm:my-name-is-url@1.4.0/dist/parser.js"
     ]
   },
 
@@ -32,7 +32,7 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
     "escape-html": "npm:escape-html@1.0.3",
-    "my-name-is-url": "npm:my-name-is-url@1.3.1",
+    "my-name-is-url": "npm:my-name-is-url@1.4.0",
     "strict-uri-encode": "npm:strict-uri-encode@1.1.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -61,9 +61,8 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:my-name-is-url@1.3.1": {
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2",
-      "tlds": "npm:tlds@1.111.0"
+    "npm:my-name-is-url@1.4.0": {
+      "tlds": "npm:tlds@1.203.1"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
